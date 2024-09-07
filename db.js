e86e7f0818a11976b733ee0528c853594a08b815
hossam <hossameltahan2004@gmail.com>
2024-09-07 22:53:04 +0300

    vercel commit

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',  // Default XAMPP username
-  password: '',  // Leave blank if there's no password
-  database: 'millionaire_game'
+  host: 'b2zyfptjbv8topy3trho-mysql.services.clever-cloud.com',
+  user: 'uwpipfr3xds97kn1',  // Default XAMPP username
+  password: 'uwpipfr3xds97kn1',  // Leave blank if there's no password
+  database: 'b2zyfptjbv8topy3trho'
 });
 
 module.exports = db;
