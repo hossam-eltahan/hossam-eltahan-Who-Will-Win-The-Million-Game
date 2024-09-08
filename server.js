65201da86c629e77b10239f8b74e3b8309611218
hossam <hossameltahan2004@gmail.com>
2024-09-08 03:04:11 +0300

    clevercloud commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./db');  // Import the database connection
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT ||5000;
 
 // Middleware
 app.use(cors());
